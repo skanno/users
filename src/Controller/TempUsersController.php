24a0140bde26b96f3cb3d8ca4ce0fe8c5c316d70
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2024-07-01 16:25:54 +0900

    feat: 不要なコードを削除

--- src/Controller/TempUsersController.php
+++ src/Controller/TempUsersController.php
@@ -25,32 +25,6 @@ class TempUsersController extends AppController
     }
 
     /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
-    public function index()
-    {
-        $query = $this->TempUsers->find();
-        $tempUsers = $this->paginate($query);
-
-        $this->set(compact('tempUsers'));
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Temp User id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view(string $id): void
-    {
-        $tempUser = $this->TempUsers->get($id, contain: []);
-        $this->set(compact('tempUser'));
-    }
-
-    /**
      * Add method
      *
      * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
@@ -89,47 +63,5 @@ class TempUsersController extends AppController
 
         $this->set('c', md5($tempUser->onetime_toke . gethostname()));
         $this->set(compact('tempUser'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Temp User id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit(string $id)
-    {
-        $tempUser = $this->TempUsers->get($id, contain: []);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $tempUser = $this->TempUsers->patchEntity($tempUser, $this->request->getData());
-            if ($this->TempUsers->save($tempUser)) {
-                $this->Flash->success(__('The temp user has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The temp user could not be saved. Please, try again.'));
-        }
-        $this->set(compact('tempUser'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Temp User id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete(string $id)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $tempUser = $this->TempUsers->get($id);
-        if ($this->TempUsers->delete($tempUser)) {
-            $this->Flash->success(__('The temp user has been deleted.'));
-        } else {
-            $this->Flash->error(__('The temp user could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
